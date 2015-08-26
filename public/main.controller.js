977dac6c7c891074e5c37d78d98db6e5d542746a
Justin Barabander <justinbarabander@Justins-MacBook-Pro.local>
2015-08-26 17:11:07 -0400

    Finished the reset button

--- public/main.controller.js
+++ public/main.controller.js
@@ -1,10 +1,40 @@
-app.controller('MainController', function ($scope, whateverName) {
-	$scope.flashCards = whateverName;
+// app.controller('MainController', function ($scope, whateverName) {
+// 	$scope.flashCards = whateverName;
+//
+// 	$scope.answerQuestion = function (answer, flashCard) {
+// 		if (!flashCard.answered) {
+// 			flashCard.answered = true;
+// 			flashCard.answeredCorrectly = answer.correct;
+// 		}
+// 	}
+// });
 
-	$scope.answerQuestion = function (answer, flashCard) {
-		if (!flashCard.answered) {
-			flashCard.answered = true;
-			flashCard.answeredCorrectly = answer.correct;
-		}
-	}
+app.controller('MainController', function ($scope, FlashCardsFactory) {
+	FlashCardsFactory.getFlashCards().then(function(result) {
+		$scope.flashCards = result;
+		// console.log($scope.flashCards);
+	});
+
+	$scope.categories = [
+    'MongoDB',
+    'Express',
+    'Angular',
+    'Node'
+	];
+
+	$scope.getCategoryCards = function(category) {
+		$scope.clickedCategory = category;
+		FlashCardsFactory.getFlashCards(category).then(function(result) {
+			$scope.flashCards = result;
+		});
+	};
+
+	// $scope.flashCards = whateverName;
+	//
+	// $scope.answerQuestion = function (answer, flashCard) {
+	// 	if (!flashCard.answered) {
+	// 		flashCard.answered = true;
+	// 		flashCard.answeredCorrectly = answer.correct;
+	// 	}
+	// }
 });
