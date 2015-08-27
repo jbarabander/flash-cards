dfb55ff0b492af9e49d031dfe196e7877e4d70ea
Justin Barabander <justinbarabander@Justins-MacBook-Pro.local>
2015-08-27 14:57:11 -0400

    Finished part 2

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,9 @@
 var app = angular.module('flashCards', []);
+
+app.filter('cheat', function(){
+  return function(collection) {
+    return collection.filter(function(element) {
+      return element.correct;
+    });
+  };
+});
